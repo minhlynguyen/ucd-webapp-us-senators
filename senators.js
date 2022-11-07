a40ed36c59d77d9ac32d5924f2d3ed8ad31a4e68
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2022-11-07 00:04:32 +0000

    update bullet 2

--- senators.js
+++ senators.js
@@ -7,12 +7,13 @@ xmlhttp.onreadystatechange = function() {
         //Parse the JSON data to a JavaScript variable. 
         var parsedObj = JSON.parse(xmlhttp.responseText);  
         // This function is defined below and deals with the JSON data parsed from the file. 
-        displayJSON(parsedObj);   
+        displayJSON(parsedObj);
+        displayLeader(parsedObj);
     }
 };
 
 function displayJSON(obj) {
-    
+
     var senators = obj.objects;
     var democratCount = 0;
     var republicanCount = 0;
@@ -35,6 +36,28 @@ function displayJSON(obj) {
     document.getElementById("id03").innerHTML = independentCount;
     
 }
+
+party = ["Democrat", "Republican", "Independent"]
+
+function displayLeader(obj){
+
+    var senators = obj.objects;
+    var out = "";
+
+    //This code iterates through the senators and display the senators with leadership role, grouped by party
+    for (var i = 0; i < party.length; i++){
+        for (var j = 0; j < senators.length; j++){
+            if (party[i] === senators[j].party && senators[j].leadership_title !== null){
+                out += '<ul>'+ senators[j].leadership_title + ': ' + senators[j].person.firstname + ' ' 
+                        + senators[j].person.lastname + ' (' + senators[j].party + ')</ul>'
+            }
+        }
+    }
+
+    document.getElementById("id04").innerHTML = out; 
+}
+
+
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
